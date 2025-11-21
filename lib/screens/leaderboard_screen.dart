acf97a803d2e4a1317beb125c2377b61e28d056c
JKRX87 <drenip001@gmail.com>
2025-11-21 08:09:09 +0300

    Update leaderboard_screen.dart

--- lib/screens/leaderboard_screen.dart
+++ lib/screens/leaderboard_screen.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import '../widgets/section_background.dart';
+
+class LeaderboardScreen extends StatelessWidget {
+  const LeaderboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SectionBackground(
+      child: Center(
+        child: Text(
+          'Рейтинг',
+          style: const TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
 
