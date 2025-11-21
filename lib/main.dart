cc4b0131000cb2861546e453e2dccb4a599476af
JKRX87 <drenip001@gmail.com>
2025-11-21 08:06:20 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'telegram_webapp.dart';
+import 'screens/home_screen.dart';
+import 'screens/games_screen.dart';
+import 'screens/leaderboard_screen.dart';
+import 'screens/friends_screen.dart';
+import 'screens/tasks_screen.dart';
 
 void main() {
   runApp(const AppRoot());
@@ -13,41 +16,7 @@ class AppRoot extends StatefulWidget {
 }
 
 class _AppRootState extends State<AppRoot> {
-  bool isTelegram = false;
-  String colorScheme = 'light';
-  Map<String, dynamic> user = {};
   int bottomIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _initWebApp();
-  }
-
-  void _initWebApp() {
-    isTelegram = TelegramWebApp.isAvailable;
-    colorScheme = TelegramWebApp.colorScheme;
-
-    final unsafe = TelegramWebApp.initDataUnsafe;
-    if (unsafe != null && unsafe['user'] != null) {
-      user = Map<String, dynamic>.from(unsafe['user']);
-    }
-
-    if (isTelegram) {
-      TelegramWebApp.ready();
-      TelegramWebApp.expand();
-      TelegramWebApp.mainButtonSetText('Отправить');
-      TelegramWebApp.mainButtonHide();
-      TelegramWebApp.mainButtonOnClick(() {
-        final payload = jsonEncode({
-          'action': 'submit',
-          'user': user,
-        });
-        TelegramWebApp.sendData(payload);
-      });
-    }
-    setState(() {});
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,8 +26,8 @@ class _AppRootState extends State<AppRoot> {
       home: Scaffold(
         body: IndexedStack(
           index: bottomIndex,
-          children: [
-            HomeScreen(user: user, isTelegram: isTelegram),
+          children: const [
+            HomeScreen(),
             GamesScreen(),
             LeaderboardScreen(),
             FriendsScreen(),
@@ -76,92 +45,6 @@ class _AppRootState extends State<AppRoot> {
             NavigationDestination(icon: Icon(Icons.checklist), label: 'Задания'),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class SectionBackground extends StatelessWidget {
-  final Widget child;
-  const SectionBackground({super.key, required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/main_bg.jpeg'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: child,
-    );
-  }
-}
-
-// ---------- Screens ----------
-
-class HomeScreen extends StatelessWidget {
-  final Map<String, dynamic> user;
-  final bool isTelegram;
-  const HomeScreen({super.key, required this.user, required this.isTelegram});
-
-  @override
-  Widget build(BuildContext context) {
-    return SectionBackground(
-      child: Center(
-        child: Text(
-          isTelegram ? 'Запущено в Telegram' : 'Открыто в браузере',
-          style: const TextStyle(color: Colors.white, fontSize: 20),
-        ),
-      ),
-    );
-  }
-}
-
-class GamesScreen extends StatelessWidget {
-  const GamesScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return SectionBackground(
-      child: Center(
-        child: Text('Игры', style: const TextStyle(color: Colors.white, fontSize: 20)),
-      ),
-    );
-  }
-}
-
-class LeaderboardScreen extends StatelessWidget {
-  const LeaderboardScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return SectionBackground(
-      child: Center(
-        child: Text('Рейтинг', style: const TextStyle(color: Colors.white, fontSize: 20)),
-      ),
-    );
-  }
-}
-
-class FriendsScreen extends StatelessWidget {
-  const FriendsScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return SectionBackground(
-      child: Center(
-        child: Text('Друзья', style: const TextStyle(color: Colors.white, fontSize: 20)),
-      ),
-    );
-  }
-}
-
-class TasksScreen extends StatelessWidget {
-  const TasksScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return SectionBackground(
-      child: Center(
-        child: Text('Задания', style: const TextStyle(color: Colors.white, fontSize: 20)),
       ),
     );
   }
