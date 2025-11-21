9257d38950676ed0c9a11d37dad079e08aa9a26d
JKRX87 <drenip001@gmail.com>
2025-11-21 08:08:05 +0300

    Update game_background.dart

--- lib/widgets/game_background.dart
+++ lib/widgets/game_background.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+
+enum GameType { blackjack, tictactoe, shell }
+
+class GameBackground extends StatelessWidget {
+  final Widget child;
+  final GameType game;
+  const GameBackground({super.key, required this.child, required this.game});
+
+  @override
+  Widget build(BuildContext context) {
+    late final String assetPath;
+    switch (game) {
+      case GameType.blackjack:
+        assetPath = 'assets/images/bg_blackjack.jpeg';
+        break;
+      case GameType.tictactoe:
+        assetPath = 'assets/images/bg_tictactoe.jpeg';
+        break;
+      case GameType.shell:
+        assetPath = 'assets/images/bg_shell.jpeg';
+        break;
+    }
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(assetPath),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
 
