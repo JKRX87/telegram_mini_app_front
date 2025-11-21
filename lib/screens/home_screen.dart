ef6135b909e483bcefbcc1e80aa332a770e52137
JKRX87 <drenip001@gmail.com>
2025-11-21 09:32:36 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import '../widgets/section_background.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final int points;
+  const HomeScreen({super.key, required this.points});
 
   @override
   Widget build(BuildContext context) {
     return SectionBackground(
       child: Center(
         child: Text(
-          'Главная',
-          style: const TextStyle(color: Colors.white, fontSize: 20),
+          'Твои очки: $points',
+          style: const TextStyle(color: Colors.white, fontSize: 24),
         ),
       ),
     );
   }
 }
-
