fed258969e73d9af35d05c1a65405ca4bc6b13ea
JKRX87 <drenip001@gmail.com>
2025-11-21 08:08:33 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import '../widgets/section_background.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SectionBackground(
+      child: Center(
+        child: Text(
+          'Главная',
+          style: const TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
 
