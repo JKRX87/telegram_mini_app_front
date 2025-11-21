0209b8713f22ea7b4210c7aca876a4a9ffb08222
JKRX87 <drenip001@gmail.com>
2025-11-21 10:41:51 +0300

    Update game_background.dart

--- lib/widgets/game_background.dart
+++ lib/widgets/game_background.dart
@@ -3,34 +3,31 @@ import 'package:flutter/material.dart';
 enum GameType { blackjack, tictactoe, shell }
 
 class GameBackground extends StatelessWidget {
-  final Widget child;
   final GameType game;
-  const GameBackground({super.key, required this.child, required this.game});
+  final Widget child;
+  const GameBackground({super.key, required this.game, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    late final String assetPath;
-    switch (game) {
-      case GameType.blackjack:
-        assetPath = 'assets/images/bg_blackjack.jpeg';
-        break;
-      case GameType.tictactoe:
-        assetPath = 'assets/images/bg_tictactoe.jpeg';
-        break;
-      case GameType.shell:
-        assetPath = 'assets/images/bg_shell.jpeg';
-        break;
-    }
-
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage(assetPath),
+          image: AssetImage(_backgroundForGame(game)),
           fit: BoxFit.cover,
         ),
       ),
       child: child,
     );
   }
-}
 
+  String _backgroundForGame(GameType game) {
+    switch (game) {
+      case GameType.blackjack:
+        return 'assets/images/blackjack.jpeg';
+      case GameType.tictactoe:
+        return 'assets/images/tictactoe.jpeg';
+      case GameType.shell:
+        return 'assets/images/shell.jpeg';
+    }
+  }
+}
