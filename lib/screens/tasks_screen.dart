4c4334fb80d77c50cc5a76350fdbf27ef734581f
JKRX87 <drenip001@gmail.com>
2025-11-21 08:09:40 +0300

    Update tasks_screen.dart

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import '../widgets/section_background.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SectionBackground(
+      child: Center(
+        child: Text(
+          'Задания',
+          style: const TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
 
