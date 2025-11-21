2c56dcb838580ade36deb1fcfa89995cf76d1c6f
JKRX87 <drenip001@gmail.com>
2025-11-21 08:10:04 +0300

    Update blackjack_screen.dart

--- lib/screens/games/blackjack_screen.dart
+++ lib/screens/games/blackjack_screen.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import '../../widgets/game_background.dart';
+
+class BlackjackScreen extends StatelessWidget {
+  const BlackjackScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GameBackground(
+      game: GameType.blackjack,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(title: const Text('Blackjack'), backgroundColor: Colors.black26),
+        body: const Center(child: Text('Здесь будет логика Blackjack', style: TextStyle(color: Colors.white))),
+      ),
+    );
+  }
+}
 
