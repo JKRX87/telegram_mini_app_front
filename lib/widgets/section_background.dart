855f5bccb0a6bf2be82573e5f14ebf3a66c091fb
JKRX87 <drenip001@gmail.com>
2025-11-21 08:06:58 +0300

    Update section_background.dart

--- lib/widgets/section_background.dart
+++ lib/widgets/section_background.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SectionBackground extends StatelessWidget {
+  final Widget child;
+  const SectionBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/main_bg.jpeg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
 
